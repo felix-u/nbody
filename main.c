303d43300e0546ae1d693891dd7552e4320c976c
felix-u <29074327+felix-u@users.noreply.github.com>
2022-05-31 23:52:37 -0400

    Control candidate circle size by scrolling

--- main.c
+++ main.c
@@ -1,59 +1,64 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Mouse input
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "raylib.h"
 
-int main(void)
-{
+#define BACKGROUND (Color){ 147, 161, 161, 255 }
+#define FOREGROUND (Color){  34,  71,  80, 255 }
+
+int main(void) {
+
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 960;
+    const int screenHeight = 540;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - mouse input");
+    InitWindow(screenWidth, screenHeight, "nbody");
 
-    Vector2 ballPosition = { -100.0f, -100.0f };
-    Color ballColor = DARKBLUE;
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60);
     //---------------------------------------------------------------------------------------
+
+    // INITIAL VARIABLES
+
+    // for scrolling to change candidate size
+    int candidateRadius = 10;
+    int scroll = 0;
+    int inputScale = 1;
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        ballPosition = GetMousePosition();
+	/* // Update */
+	/* //---------------------------------------------------------------------------------- */
 
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) ballColor = MAROON;
-        else if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) ballColor = LIME;
-        else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) ballColor = DARKBLUE;
-        else if (IsMouseButtonPressed(MOUSE_BUTTON_SIDE)) ballColor = PURPLE;
-        else if (IsMouseButtonPressed(MOUSE_BUTTON_EXTRA)) ballColor = YELLOW;
-        else if (IsMouseButtonPressed(MOUSE_BUTTON_FORWARD)) ballColor = ORANGE;
-        else if (IsMouseButtonPressed(MOUSE_BUTTON_BACK)) ballColor = BEIGE;
-        //----------------------------------------------------------------------------------
+	/* if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) ballColor = MAROON; */
+	/* else if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) ballColor = LIME; */
+	/* else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) ballColor = DARKBLUE; */
+	/* else if (IsMouseButtonPressed(MOUSE_BUTTON_SIDE)) ballColor = PURPLE; */
+	/* else if (IsMouseButtonPressed(MOUSE_BUTTON_EXTRA)) ballColor = YELLOW; */
+	/* else if (IsMouseButtonPressed(MOUSE_BUTTON_FORWARD)) ballColor = ORANGE; */
+	/* else if (IsMouseButtonPressed(MOUSE_BUTTON_BACK)) ballColor = BEIGE; */
+	/* //---------------------------------------------------------------------------------- */
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+	// Draw
+	//----------------------------------------------------------------------------------
+	BeginDrawing();
+	ClearBackground(BACKGROUND);
 
-            ClearBackground(RAYWHITE);
+	// control curve - when candidate radius smaller, scroll makes smaller
+	// adjustment. when radius larger, scroll makes larger adjustment.
+	scroll = GetMouseWheelMove();
+	if (GetMouseWheelMove()) {
+	    inputScale = candidateRadius / 10;
+	    if (inputScale < 1) inputScale = 1;
+	    candidateRadius += (scroll * inputScale);   
+	    if (candidateRadius < 5) candidateRadius = 5;
+	}
+	DrawCircleV(GetMousePosition(), candidateRadius, FOREGROUND);
 
-            DrawCircleV(ballPosition, 40, ballColor);
+	/* DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, FOREGROUND); */
 
-            DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, DARKGRAY);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+	EndDrawing();
+	//----------------------------------------------------------------------------------
     }
 
     // De-Initialization
