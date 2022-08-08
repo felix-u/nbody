826adb4fc46fda8cc369943debe3fdf061d0cd6b
felix-u <29074327+felix-u@users.noreply.github.com>
2022-08-08 13:23:34 -0400

    Includes

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
+// exact-width integer types
+// https://en.wikibooks.org/wiki/C_Programming/inttypes.h
+#include <inttypes.h>
 #include <stdio.h>
+// return explicit error codes
 #include <error.h>
+#include <SDL2/SDL.h>
 
 int main() {
 
