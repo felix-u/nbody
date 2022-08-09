1a732540ba179358f33d6649faae84d29b765fae
felix-u <29074327+felix-u@users.noreply.github.com>
2022-08-09 12:32:50 -0400

    Window renders natively through wayland

--- shell.nix
+++ shell.nix
@@ -1,9 +1,6 @@
 # shell.nix
 
 { pkgs ? import <nixpkgs> {} }:
-let
-    pkgs-unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
-in
 with pkgs; mkShell {
   nativeBuildInputs = [
     # pkgconfig
@@ -13,7 +10,8 @@ with pkgs; mkShell {
     # xorg.libX11 xorg.libXcursor xorg.libXrandr xorg.libXi # To use x11 feature
 
     # To use wayland feature
-    egl-wayland libdecor libxkbcommon wayland wayland-scanner wayland-utils
-    wayland-protocols pkgs-unstable.SDL2
+    # egl-wayland libdecor libxkbcommon wayland wayland-scanner wayland-utils
+    # wayland-protocols
+    SDL2
   ];
 }
--- src/main.c
+++ src/main.c
@@ -7,59 +7,41 @@
 // return explicit error codes
 #include <error.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_video.h>
 #include <SDL2/SDL_events.h>
-#include <SDL2/SDL_pixels.h>
-#include <SDL2/SDL_render.h>
-#include <SDL2/SDL_timer.h>
-#include <SDL2/SDL_hints.h>
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 #define LOGICAL_WIDTH 320
 #define LOGICAL_HEIGHT 180
 
-// FIXME
-// obviously I'm doing something here wrong, since the C99 example from
-// https://github.com/xyproto/sdl2-examples works with wayland, and this
-// doesn't
-
 
 int main() {
 
-    SDL_version compiled;
-    SDL_version linked;
-    SDL_VERSION(&compiled);
-    SDL_GetVersion(&linked);
-    printf("We compiled against SDL version %u.%u.%u ...\n",
-           compiled.major, compiled.minor, compiled.patch);
-    printf("But we are linking against SDL version %u.%u.%u.\n",
-           linked.major, linked.minor, linked.patch);
-
     // initialise SDL
-    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "wayland");
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-            fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
-            return EXIT_FAILURE;
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
 	}
 
     // create window
-    SDL_Window* window = SDL_CreateWindow(
-        "nbody",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        SCREEN_WIDTH, SCREEN_HEIGHT,
+    // NOTE: Using SDL_WINDOWPOS_CENTERED will *VERY STUPIDLY* silently force
+    //       X11 over Wayland, because Wayland doesn't expose window
+    //       positioning functionality to clients. So I'm not using it...
+    //       OBVIOUSLY.
+	SDL_Window* window = SDL_CreateWindow(
+        "nbody", // title
+        100, 100, // co-ordinates (see note above)
+        SCREEN_WIDTH, SCREEN_HEIGHT, // dimensions
         SDL_WINDOW_SHOWN
     );
-    if (window == NULL) {
-        fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
-            return EXIT_FAILURE;
-    }
+	if (window == NULL) {
+		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+	}
 
-    // create renderer, which sets up hardware acceleration
-    SDL_Renderer* renderer = SDL_CreateRenderer(
-        window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-    );
-    if (renderer == NULL) {
+    // create renderer
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == NULL) {
 		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
 		SDL_DestroyWindow(window);
 		SDL_Quit();
@@ -67,10 +49,9 @@ int main() {
 	}
     SDL_RenderSetLogicalSize(renderer, LOGICAL_WIDTH, LOGICAL_HEIGHT);
 
-    // event loop
+    // NOTE: Can't use SDL_Event because VIDEODRIVER=x11 will be forced
     SDL_bool done = SDL_FALSE;
     while (!done) {
-        SDL_Event event;
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 32, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
@@ -78,21 +59,12 @@ int main() {
         SDL_SetRenderDrawColor(renderer, 144, 48, 160, SDL_ALPHA_OPAQUE);
         SDL_RenderDrawLine(renderer, 20, 40, 20, 80);
 
-        SDL_RenderPresent(renderer);
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                done = SDL_TRUE;
-            }
-        }
-    }
+		SDL_RenderPresent(renderer);
+	}
 
-    if (renderer) {
-        SDL_DestroyRenderer(renderer);
-    }
-    if (window) {
-        SDL_DestroyWindow(window);
-    }
-    SDL_Quit();
-    return EXIT_SUCCESS;
+    if (renderer) SDL_DestroyRenderer(renderer);
+	if (window) SDL_DestroyWindow(window);
+	SDL_Quit();
+	return EXIT_SUCCESS;
 }
 
